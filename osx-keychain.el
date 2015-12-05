e8a65bdef44903a482ce37d56653424ec5d7f09c
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2015-12-05 17:12:51 +0100

    Simplify version-specific arguments

--- osx-keychain.el
+++ osx-keychain.el
@@ -1,39 +1,29 @@
-(defun os-x-version ()
-  "Returns whether we are running OS X Lion of Mountain Lion"
-  (let ((major-version-number (substring (shell-command-to-string "sw_vers -productVersion") 0 5)))
-    (cond
-     ((string-equal major-version-number "10.7.") 'os-x-lion)
-     ((string-equal major-version-number "10.8.") 'os-x-mountain-lion))))
+(defun security-args (type account server)
+  "Build a list of arguments for `security', the keychain application of OS X."
+  (let ((version-string (shell-command-to-string "sw_vers -productVersion"))
+        (args '()))
+    (add-to-list 'args (concat "find-" type "-password") t)
+    (when (string-match "\\([0-9]+\\)\\.\\([0-9]+\\)\\.\\([0-9]+\\)" version-string)
+      (let ((major-version (string-to-number (match-string 1 version-string)))
+            (minor-version (string-to-number (match-string 2 version-string)))
+            (patch-version (string-to-number (match-string 3 version-string))))
+        (when (= major-version 10)
+          (if (<= minor-version 7)
+              (add-to-list 'args "-w" t)
+            (add-to-list 'args "-g" t)))))
+    (add-to-list 'args "-a" t) (add-to-list 'args account t)
+    (add-to-list 'args "-s" t) (add-to-list 'args server t)
+    args))
 
-(defun find-keychain-password-mountain-lion (type account server)
-  (condition-case nil
-      (first
-       (process-lines
-	"security" (concat "find-" type "-password")
-	"-w" ; display only the password to stdout
-	"-a" account
-	"-s" server))
-    ; TODO: handle case when keychain does not return an entry
-    (error "")))
-
-(defun find-keychain-password-lion (type account server)
-  (let ((password-line (first
-			(process-lines
-			 "security"  (concat "find-" type "-password")
-			 "-g" ; display only the password to stdout
-			 "-a" account
-			 "-s" server
-			 ))))
+(defun find-keychain-password (type account server)
+  (let ((password-line
+         (first
+          (apply 'process-lines
+                 (append '("security")
+                         (security-args type account server))))))
     (string-match "password: \"\\(.*\\)\"" password-line)
     (match-string 1 password-line)))
 
-(defun find-keychain-password (type account server)
-  (let ((os-x-version (os-x-version)))
-    (cond
-     ((eq os-x-version 'os-x-lion)
-      (find-keychain-password-lion type account server))
-     ((eq os-x-version 'os-x-mountain-lion)
-      (find-keychain-password-mountain-lion type account server)))))
 
 (defun find-keychain-internet-password (account server)
   (find-keychain-password "internet" account server))
